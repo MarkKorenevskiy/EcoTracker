bde1b49574dff407b4926a5bc664aa9fbe9bb419
iana <112834476+justyanaa@users.noreply.github.com>
2023-11-29 09:53:32 -0500

    added Layout and some styles

---
+++ src/components/Layout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+
+const Layout = ({ children }) => {
+    return (
+        <View style={styles.container}>
+            {children}
+            <Image
+                source={require('../../assets/save-the-planet.png')}
+                style={styles.bottomImage}
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#abbd8c',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    bottomImage: {
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        height: '50%',
+        opacity: 0.07,
+    }
+});
+
+export default Layout;
