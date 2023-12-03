5e4918de4482bcc1d974199aeb39f37d75bafccb
MarkKorenevskiy <71618368+MarkKorenevskiy@users.noreply.github.com>
2023-12-03 14:15:59 -0500

    Added saving of tracker result, displaying latest result on personal page screen

--- src/components/carbonFootprintTracker/CarbonTracker.js
+++ src/components/carbonFootprintTracker/CarbonTracker.js
@@ -4,6 +4,7 @@ import CarCalculator from "./CarCalculator";
 import PublicCalculator from "./PublicCalculator";
 import FlightCalculator from "./FlightCalculator";
 import MotorbikeCalculator from "./MotorbikeCalculator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,15 +12,27 @@ function EmptyScreen() {
     return (<View><Text>Empty screen</Text></View>)
 }
 
+export async function saveResultToStorage(result) {
+    try {
+        const jsonString = await AsyncStorage.getItem("tracker_results");
+        const currentResults = jsonString != null ? JSON.parse(jsonString) : [];
+        currentResults.push(result);
+        await AsyncStorage.setItem("tracker_results", JSON.stringify(currentResults));
+        console.log("Tracker result added to AsyncStorage");
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
 export default function CarbonTracker() {
     return (
 
         <Tab.Navigator initialRouteName={"Home"}>
-            <Tab.Screen name={"Flights"} component={FlightCalculator} options={{ headerShown: false }} />
-            <Tab.Screen name={"Car"} component={CarCalculator} options={{ headerShown: false }} />
-            <Tab.Screen name={"Home"} component={EmptyScreen} options={{ headerShown: false }} />
-            <Tab.Screen name={"Motorbike"} component={MotorbikeCalculator} options={{ headerShown: false }} />
-            <Tab.Screen name={"Public"} component={PublicCalculator} options={{ headerShown: false }} />
+            <Tab.Screen name={"Flights"} component={FlightCalculator} options={{headerShown: false}}/>
+            <Tab.Screen name={"Car"} component={CarCalculator} options={{headerShown: false}}/>
+            <Tab.Screen name={"Home"} component={EmptyScreen} options={{headerShown: false}}/>
+            <Tab.Screen name={"Motorbike"} component={MotorbikeCalculator} options={{headerShown: false}}/>
+            <Tab.Screen name={"Public"} component={PublicCalculator} options={{headerShown: false}}/>
         </Tab.Navigator>
 
     );
